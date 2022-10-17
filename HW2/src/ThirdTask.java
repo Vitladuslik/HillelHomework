c9b3177c1304ee649acd7369ee1d77bf5e8556b5
Vitladuslik <vitladuslik@gmail.com>
2022-10-17 22:08:19 +0300

    Remove output method, add math logic

--- HW2/src/ThirdTask.java
+++ HW2/src/ThirdTask.java
@@ -7,20 +7,29 @@ public class ThirdTask {
     static double depositAmount;
     static double annualRate;
     static int years;
-    public static void main(String[] args) {
+    static int counter;
+
+    public static void main(String [] args) {
 
         depositAmount = Integer.parseInt(args[0]);
         annualRate = Double.parseDouble(args[1]);
         years = Integer.parseInt(args[2]);
         doMath();
-        output();
 
     }
 
     static void doMath() {
-
-    }
-    static void output() {
-
+        System.out.println("Deposit amount = " + depositAmount + " UAH.");
+        System.out.println("Annual rate = " + annualRate + "%.");
+        System.out.println("Years = " + years);
+        System.out.println("--------------------------------");
+        counter = 1;
+        while (counter <= years) {
+            System.out.println("Year " + counter);
+            depositAmount*=(1+(annualRate/100));
+            System.out.printf("Deposit amount =  %.2f UAH \n", depositAmount);
+            System.out.println("--------------------------------");
+            counter++;
+        }
     }
 }
