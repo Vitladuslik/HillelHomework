bae13b97b7f34cf6150c24d1b2c4321eb4de7813
Vitladuslik <vitladuslik@gmail.com>
2022-10-17 21:34:14 +0300

    Add variables

--- HW2/src/ThirdTask.java
+++ HW2/src/ThirdTask.java
@@ -1,3 +1,13 @@
 public class ThirdTask {
 
+    /**
+     * @author Vitalii Kirenkin
+     */
+
+    static double depositAmount;
+    static double annualRate;
+    static int years;
+    public static void main(String[] args) {
+
+    }
 }
