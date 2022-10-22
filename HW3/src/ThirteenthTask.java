6ff3dc9bcd20953176f5363346666e8559fe9c3e
Vitladuslik <vitladuslik@gmail.com>
2022-10-22 23:03:51 +0300

    HW3 Task 13 done

---
+++ HW3/src/ThirteenthTask.java
@@ -0,0 +1,33 @@
+public class ThirteenthTask {
+
+    static int line;
+
+    public static void main(String[] args) {
+
+        String[][] chess = new String[][]{
+                {"W", "B", "W", "B", "W", "B", "W", "B"},
+                {"B", "W", "B", "W", "B", "W", "B", "W"},
+                {"W", "B", "W", "B", "W", "B", "W", "B"},
+                {"B", "W", "B", "W", "B", "W", "B", "W"},
+                {"W", "B", "W", "B", "W", "B", "W", "B"},
+                {"B", "W", "B", "W", "B", "W", "B", "W"},
+                {"W", "B", "W", "B", "W", "B", "W", "B"},
+                {"B", "W", "B", "W", "B", "W", "B", "W"}
+        };
+
+        for (int x = 0; x < 9; x++) {
+            if (x == 8) {
+                if (line == 7) {
+                    break;
+                } else {
+                    line++;
+                    x = 0;
+                    System.out.println();
+                }
+            }
+            System.out.print(chess[line][x] + "  ");
+        }
+
+    }
+
+}
