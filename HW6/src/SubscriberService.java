4c8f6f68281a52102d6de2bc1f4a2588e7d02f46
Vitladuslik <vitladuslik@gmail.com>
2022-11-02 21:06:19 +0200

    HW6 structure marked

---
+++ HW6/src/SubscriberService.java
@@ -0,0 +1,12 @@
+public interface SubscriberService {
+    void findSubscriber();
+
+    void findSubscribersUsedIntercity();
+
+    void findSubscribersByFirstLetter();
+
+    void calculateCityTraffic();
+
+    void findSubscribersWithDebt();
+
+}
---
+++ HW6/src/SubscriberServiceImpl.java
@@ -0,0 +1,27 @@
+public class SubscriberServiceImpl implements SubscriberService{
+
+    @Override
+    public void findSubscriber() {
+
+    }
+
+    @Override
+    public void findSubscribersByFirstLetter() {
+
+    }
+
+    @Override
+    public void findSubscribersUsedIntercity() {
+
+    }
+
+    @Override
+    public void findSubscribersWithDebt() {
+
+    }
+
+    @Override
+    public void calculateCityTraffic() {
+
+    }
+}
