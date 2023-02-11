6020b7e0287d722c6e22c42b80bda0b0404a90c8
Vitladuslik <vitladuslik@gmail.com>
2023-02-12 01:41:06 +0200

    HW 24 tests updated

--- src/HW24/Hibernate/src/main/java/DatabaseHandlerImpl.java
+++ src/HW24/Hibernate/src/main/java/DatabaseHandlerImpl.java
@@ -19,7 +19,6 @@ public class DatabaseHandlerImpl implements DatabaseHandler {
             session.beginTransaction();
             session.save(student);
             session.getTransaction().commit();
-//            HibernateService.shutdown();
 
             return true;
 
@@ -34,7 +33,7 @@ public class DatabaseHandlerImpl implements DatabaseHandler {
 
         List<Student> result;
 
-//        try {
+        try {
 
             Session session = HibernateService.getSessionFactory().openSession();
 
@@ -47,15 +46,14 @@ public class DatabaseHandlerImpl implements DatabaseHandler {
 
             result = allQuery.getResultList();
 
-//            HibernateService.shutdown();
 
             return result;
 
-//        } catch (Exception e) {
-//            System.err.println("Error!");
-//            HibernateService.shutdown();
-//            return null;
-//        }
+        } catch (Exception e) {
+            System.err.println("Error!");
+            HibernateService.shutdown();
+            return null;
+        }
     }
 
     @Override
@@ -72,12 +70,10 @@ public class DatabaseHandlerImpl implements DatabaseHandler {
             result = (List<Student>) criteria.list();
 
 
-//            HibernateService.shutdown();
 
             return result;
         } catch (Exception e) {
             System.err.println("Error!");
-            HibernateService.shutdown();
             return null;
         }
 
@@ -101,7 +97,6 @@ public class DatabaseHandlerImpl implements DatabaseHandler {
                 }
             }
 
-//            HibernateService.shutdown();
 
             return result;
 
--- src/HW24/Hibernate/src/test/java/DatabaseHandlerImplTest.java
+++ src/HW24/Hibernate/src/test/java/DatabaseHandlerImplTest.java
@@ -1,4 +1,6 @@
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,27 +13,39 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class DatabaseHandlerImplTest {
 
+    private static SessionFactory sessionFactory = null;
+    private static Student testStudent = new Student("Test Testov", 1990, 12);
+
     @Test
     public void addStudent() {
 
         DatabaseHandlerImpl service = new DatabaseHandlerImpl();
-
-        Student testStudent = new Student();
-        testStudent.setName("Test Testov");
-        testStudent.setApplied(1990);
-        testStudent.setId(1112);
+        Session session = sessionFactory.openSession();
 
         assertTrue(service.addStudent(testStudent));
+
+
+        session.beginTransaction();
+        session.delete( testStudent );
+        session.getTransaction().commit();
+        session.close();
 
     }
 
 
     @Test
     public void getAll() {
-
         DatabaseHandlerImpl service = new DatabaseHandlerImpl();
+        Session session = sessionFactory.openSession();
+        service.addStudent(testStudent);
+
         assertNotNull(service.getAll());
         assertNotEquals(0, service.getAll().size());
+
+        session.beginTransaction();
+        session.delete( testStudent );
+        session.getTransaction().commit();
+        session.close();
 
     }
 
@@ -39,8 +53,15 @@ public class DatabaseHandlerImplTest {
     public void getByName() {
 
         DatabaseHandlerImpl service = new DatabaseHandlerImpl();
+        Session session = sessionFactory.openSession();
+        service.addStudent(testStudent);
 
         assertEquals(1, service.getByName("Test Testov").size());
+
+        session.beginTransaction();
+        session.delete( testStudent );
+        session.getTransaction().commit();
+        session.close();
 
     }
 
@@ -55,5 +76,13 @@ public class DatabaseHandlerImplTest {
         assertEquals(1, service.getByIds(ids).size());
 
     }
+    @BeforeEach
+    public void setUp() {
+        sessionFactory = new Configuration().configure().buildSessionFactory();
+    }
 
+    @AfterEach
+    public void tearDown() {
+        sessionFactory.close();
+    }
 }
